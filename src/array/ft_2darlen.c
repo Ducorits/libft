3831e255ea10e2560f568068e533b14f47955a6b
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2022-06-22 14:40:13 +0200

    Better description for ft_2darlen

--- src/array/ft_2darlen.c
+++ src/array/ft_2darlen.c
@@ -6,10 +6,16 @@
 /*   By: dritsema <dritsema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/22 14:18:41 by dritsema      #+#    #+#                 */
-/*   Updated: 2022/06/22 14:18:46 by dritsema      ########   odam.nl         */
+/*   Updated: 2022/06/22 14:39:26 by dritsema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Takes a NULL terminated 2d array and returns
+ * the amount of arrays in it.
+ *
+ * @param pointer The 2d array to measure.
+ * @return The total "length" of the 2d array. */
 int	ft_2darlen(void **pointer)
 {
 	int	i;
